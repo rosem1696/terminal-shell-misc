7ad2be46b80e5a883ce28da2192a4951a730dfe4
Mitchell Rose <mitchell.rose@generac.com>
2024-01-09 18:34:21 -0600

    fix download theme file correctly

--- bash/install_ohmyposh.sh
+++ bash/install_ohmyposh.sh
@@ -10,12 +10,12 @@ curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/ohmyposh
 # Download theme
 echo "Downloading Power Level 10k Rainbow Theme"
 pushd ~/ohmyposh >/dev/null
-curl -O https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/powerlevel10k_rainbow.omp.json
+curl -O https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/powerlevel10k_rainbow.omp.json
 popd >/dev/null
 
 # Set in profile or bashrc
 echo "
 # Start ohmyposh
 export PATH=\$PATH:~/ohmyposh
-eval \"\$(~/ohmyposh/oh-my-posh init bash --config ~/ohmyposh/powerlevel10k_rainbow.omp.json)\"
+eval \"\$(oh-my-posh init bash --config ~/ohmyposh/powerlevel10k_rainbow.omp.json)\"
 " >>~/.bashrc
